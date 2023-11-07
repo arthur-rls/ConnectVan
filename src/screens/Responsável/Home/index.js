8ae725fa83c7475410274d47aa841b9754c63efb
arthur-rls <ruhtra.rls@gmail.com>
2023-11-07 14:04:32 -0300

    a

--- src/screens/Responsável/Home/index.js
+++ src/screens/Responsável/Home/index.js
@@ -5,6 +5,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 import {db, auth} from '../../../firebase/config';
 import {View, Text,Image,  TouchableOpacity, TextInput, Modal, ScrollView, Keyboard, Linking} from 'react-native'
 import { doc, getDoc, onSnapshot, getDocs, collection, collectionGroup, query, where, updateDoc} from 'firebase/firestore';
+import Calendar from 'react-native-calendars/src/calendar'
  
 
 //fazer home pra nenhum motorista
@@ -14,6 +15,19 @@ export default function RHome ({route, navigation}) {
     "JULHO", "AGOSTO", "SETEMBRO", "OUTUBRO", "NOVEMBRO", "DEZEMBRO"];
     const [motorista, setMotorista] = useState('')
     const [rec, setRec] = useState('')
+    const [modalVisible, setModalVisible] = useState(false);
+    const selected = [];
+    const [dia, setDia] = useState([])
+    const onDayPress = (day) => {
+        selected.push(day.dateString)
+        console.log(selected)
+        setDia(selected)
+      };
+      const [selectedRedDates, setSelectedRedDates] = useState([]);
+
+        const onSaveButtonClick = () => {
+        
+        };
     useEffect(()=>{
         navigation.addListener('focus', () => {
         var date = new Date().getDate(); //Current Date
@@ -25,20 +39,22 @@ export default function RHome ({route, navigation}) {
             const docRef = doc(db, 'responsavel', user.uid)
             const snapshot = await getDoc(docRef)
             setRec(snapshot.data())
-            const moto = rec.motorista
-            console.log(moto)
             if(rec.motorista != undefined && rec.motorista != null && rec.motorista != ''){
-                const docRef2 = doc(db, 'motorista', moto)
+                const docRef2 = doc(db, 'motorista', rec.motorista)
                 const snapshot2 = await getDoc(docRef2)
                 const dado2 = snapshot2.data()
                 setMotorista(dado2)
             }
         })
+        
     })
     },[])
 
     const acompanhar=()=>{
         Linking.openURL(motorista.rota)
+    }
+    if(!rec || rec == undefined || rec == ''){
+        return null
     }
     return(
         <View style={styles.container}>
@@ -61,7 +77,7 @@ export default function RHome ({route, navigation}) {
                 <Text style={{ fontSize: 20, margin:'6%', fontFamily:'AileronH' }}>
                     {currentDate}
                 </Text>
-                {motorista?(
+                {rec.motorista?(
                 <View style={styles.saldot}>
                     <View style={styles.fundoSaldo}>
                         <View style={styles.viewMae}/>
@@ -85,7 +101,7 @@ export default function RHome ({route, navigation}) {
                                     </Text>
                                     <View style={{width:'30%', justifyContent:'center', height:'100%', alignItems:'center'}}>
                                         <Image source={require('../../../../assets/gradient.png')} style={styles.gradient} /> 
-                                        {motorista.pago?(
+                                        {rec.pago?(
                                             <Text
                                             style={{
                                             fontSize: 16,
@@ -134,6 +150,14 @@ export default function RHome ({route, navigation}) {
                         )}
                     </View>
                 </View>
+                <View style={styles.viewBotao}>
+                    <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.botaoMaps}>
+                        <Image source={require('../../../../assets/gradient.png')} style={[styles.gradient, {position:'absolute'}]}/>
+                        <Ionicons name="calendar" size={24} color="black" />
+                        <Text style={{fontSize:16, fontWeight:'bold', marginLeft:'5%'}}>Calendário</Text>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity onPress={()=>console.log(dia)}><Text>aa</Text></TouchableOpacity>
                 {motorista.viajando?(
                 <View style={styles.viewBotao}>
                     <TouchableOpacity onPress={() => acompanhar()} style={styles.botaoMaps}>
@@ -146,28 +170,53 @@ export default function RHome ({route, navigation}) {
                 </View>
             ):(
                     <View style={styles.fundoTab}>
-                  <View style={styles.fundoTab}>
-                    <View style={{height:'10%', width:'50%'}}>
-                      <Image
-                        source={require('../../../../assets/avan.png')}
-                        style={{width:'100%', height:'100%'}}
-                      />
+                        <View style={styles.fundoTab}>
+                            <View style={{height:'10%', width:'50%'}}>
+                            <Image
+                                source={require('../../../../assets/avan.png')}
+                                style={{width:'100%', height:'100%'}}
+                            />
+                            </View>
+                            <View style={{marginTop:'3%'}}>
+                            <Text style={{fontSize:18, fontFamily:'AileronH', color:'gray', textAlign:'center'}}>Oops! Ainda não há</Text>
+                            <Text style={{fontSize:18, fontFamily:'AileronH', color:'gray', textAlign:'center'}}>nenhum motorista</Text>
+                            <Text style={{fontSize:18, fontFamily:'AileronH', color:'gray', textAlign:'center'}}>contratado.</Text>
+                            </View>
+                            <View style={styles.viewBotao}>
+                            <TouchableOpacity style={styles.botao} onPress={()=>navigation.navigate('Pesquisar')}>
+                                <Image source={require('../../../../assets/gradient.png')} style={styles.gradient}/>
+                                <Text style={{fontSize:16, fontFamily:'AileronH', position:'absolute'}}>Contratar</Text>
+                            </TouchableOpacity>
+                            </View>
+                        </View>      
                     </View>
-                    <View style={{marginTop:'3%'}}>
-                      <Text style={{fontSize:18, fontFamily:'AileronH', color:'gray', textAlign:'center'}}>Oops! Ainda não há</Text>
-                      <Text style={{fontSize:18, fontFamily:'AileronH', color:'gray', textAlign:'center'}}>nenhum motorista</Text>
-                      <Text style={{fontSize:18, fontFamily:'AileronH', color:'gray', textAlign:'center'}}>contratado.</Text>
-                    </View>
-                    <View style={styles.viewBotao}>
-                      <TouchableOpacity style={styles.botao} onPress={()=>navigation.navigate('Pesquisar')}>
-                        <Image source={require('../../../../assets/gradient.png')} style={styles.gradient}/>
-                        <Text style={{fontSize:16, fontFamily:'AileronH', position:'absolute'}}>Contratar</Text>
-                      </TouchableOpacity>
-                    </View>
-                  </View>      
-                </View>
+
             )}
             </View>
+            <Modal visible={modalVisible}>
+                <Calendar hideArrows={false} enableSwipeMonths={true} 
+                onDayPress={onDayPress}
+                markedDates={{
+                  ...selected,
+                  // Marca os dias da semana em verde
+                  '2023-11-01': { selected: true, selectedColor: 'green' },
+                  '2023-11-02': { selected: true, selectedColor: 'green' },
+                  '2023-11-03': { selected: true, selectedColor: 'green' },
+                  '2023-11-04': { selected: true, selectedColor: 'green' },
+                  '2023-11-05': { selected: true, selectedColor: 'green' },
+                  // Marca os sábados e domingos em vermelho
+                  '2023-11-06': { selected: true, selectedColor: 'red' },
+                  '2023-11-12': { selected: true, selectedColor: 'red' },
+                  '2023-11-13': { selected: true, selectedColor: 'red' },
+                  '2023-11-19': { selected: true, selectedColor: 'red' },
+                  '2023-11-20': { selected: true, selectedColor: 'red' },
+                  '2023-11-26': { selected: true, selectedColor: 'red' },
+                  '2023-11-27': { selected: true, selectedColor: 'red' },
+                  
+                }}
+                />
+                <TouchableOpacity onPress={()=>setModalVisible(false)}><Text>aa</Text></TouchableOpacity>      
+            </Modal>
             
         </View>
     ) 
